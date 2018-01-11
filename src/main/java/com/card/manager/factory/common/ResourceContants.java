91aa01774232ac3558610414d2d48ee56a47bdbb
jsbhb <306494983@qq.com>
2018-01-11 15:52:35 +0800

    sadf

---
+++ src/main/java/com/card/manager/factory/common/ResourceContants.java
@@ -0,0 +1,25 @@
+/**  
+ * Project Name:cardmanager  
+ * File Name:ResourceContants.java  
+ * Package Name:com.card.manager.factory.common  
+ * Date:Jan 10, 20184:15:26 PM  
+ *  
+ */
+package com.card.manager.factory.common;
+
+/**
+ * ClassName: ResourceContants <br/>
+ * Function: 资源常量值. <br/>
+ * date: Jan 10, 2018 4:15:26 PM <br/>
+ * 
+ * @author hebin
+ * @version
+ * @since JDK 1.7
+ */
+public class ResourceContants {
+
+	public static final String RESOURCE_BASE_PATH = "/opt/static";
+	public static final String CMS = "cms";
+	public static final String IMAGE = "images";
+	public static final String HTML = "html";
+}
