15567effe25aff7b2433f6da2def53577309290e
luciferwqy <190873411@163.com>
2018-10-19 11:22:50 +0800

    commit

--- src/main/java/com/card/manager/factory/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/card/manager/factory/order/service/impl/OrderServiceImpl.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.management.RuntimeErrorException;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
