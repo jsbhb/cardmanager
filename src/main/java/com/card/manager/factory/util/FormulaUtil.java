6c87aefaa5ea1a4b029b9f376fe3914e2650abf8
luciferwqy <190873411@163.com>
2018-11-13 14:26:37 +0800

    commit 返佣计算公式，如果返佣小于0，则固定为0

--- src/main/java/com/card/manager/factory/util/FormulaUtil.java
+++ src/main/java/com/card/manager/factory/util/FormulaUtil.java
@@ -10,22 +10,25 @@ import javax.script.ScriptException;
 import com.card.manager.factory.log.LogUtil;
 
 public class FormulaUtil {
-	
+
 	private static final String FUNCTION_NAME = "calRebate";
 	private static final String FUNCTION_PRE = "function calRebate(rebate){return ";
 	private static final String FUNCTION_FIX = "}";
 
-	public static double calRebate(String formula, double rebate) throws ScriptException, NoSuchMethodException{
-		LogUtil.writeLog("返佣公式="+formula);
+	public static double calRebate(String formula, double rebate) throws ScriptException, NoSuchMethodException {
+		LogUtil.writeLog("返佣公式=" + formula);
 		ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
-		Compilable compEngine = (Compilable) engine;  
-	    CompiledScript script = compEngine  
-	            .compile(FUNCTION_PRE + formula + FUNCTION_FIX);  
-	    script.eval();  
-	    Invocable invoke = (Invocable) engine;  
-	    Object num = invoke.invokeFunction(FUNCTION_NAME, rebate);  
-	    Double result = Double.valueOf(num.toString());
-	    LogUtil.writeLog("返佣结果="+result);
-	    return result;
+		Compilable compEngine = (Compilable) engine;
+		CompiledScript script = compEngine.compile(FUNCTION_PRE + formula + FUNCTION_FIX);
+		script.eval();
+		Invocable invoke = (Invocable) engine;
+		Object num = invoke.invokeFunction(FUNCTION_NAME, rebate);
+		Double result = Double.valueOf(num.toString());
+		LogUtil.writeLog("返佣结果=" + result);
+		return result < 0 ? 0 : result;
+	}
+
+	public static void main(String[] args) throws NoSuchMethodException, ScriptException {
+		System.out.println(calRebate("rebate-0.5", 0.2));
 	}
 }
